511f628804850d7add78fe35c9071f1e3a026a29
MortalSecond <irondiego_jkl@hotmail.com>
2025-09-05 20:16:25 -0600

    Made the tooltips button reusable and centralized.
    - Added the "Resources" folder. This is where structural classes will go in.
    - Added the "Tooltips.cs" class, where all the tooltips' texts will be stored in.
    - Added the "TooltipButton.xaml" UserControl. The basis for all tooltips from now on.
    - Populated Tooltips.cs with some initial entries.
    - Added a test tooltip button to the server.properties sidebar.

---
+++ MinecraftServerTool/Controls/TooltipButton.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace MinecraftServerTool.Controls
+{
+    public partial class TooltipButton : UserControl
+    {
+        public TooltipButton()
+        {
+            InitializeComponent();
+        }
+        public string TooltipText
+        {
+            get { return (string)GetValue(TooltipTextProperty); }
+            set { SetValue(TooltipTextProperty, value); }
+        }
+
+        public static readonly DependencyProperty TooltipTextProperty =
+            DependencyProperty.Register(nameof(TooltipText), typeof(string), typeof(TooltipButton),
+                new PropertyMetadata(string.Empty, OnTooltipTextChanged));
+
+        private static void OnTooltipTextChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is TooltipButton tb)
+            {
+                tb.PART_TooltipText.Text = e.NewValue?.ToString();
+            }
+        }
+    }
+}
---
+++ MinecraftServerTool/Resources/Tooltips.cs
@@ -0,0 +1,12 @@
+﻿namespace MinecraftServerTool.Resources
+{
+    public static class Tooltips
+    {
+        public const string AllowFlight = "Determines whether players moving at really high speeds is allowed, or if it causes the player to be kicked from the server. Note that some mods require this to be on due to fast movement, like Create: Protection Pixel's jetpacks.";
+
+        public const string OnlineMode = "When enabled, the server verifies players with Mojang’s authentication servers. Disabling it will allow players with cracked Minecraft to join your server.";
+
+        public const string Pvp = "If enabled, players can damage each other.";
+
+    }
+}
